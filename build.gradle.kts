285e80a0636d8c98e78ca22f6e598becaac6aaff
Léo Millon <leo.millon@ekino.com>
2020-09-02 10:26:36 +0200

    chore: Upgrade Gradle IntelliJ plugin to 0.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.3.70"
-  id("org.jetbrains.intellij") version "0.4.16"
+  id("org.jetbrains.intellij") version "0.4.21"
 }
 
 intellij {
